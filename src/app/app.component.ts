28944f2b2b73d122cdf1d44686cbd7a93e0a1ca5
leandromelolm <leandrosiqueira.pe@gmail.com>
2022-01-08 10:14:00 -0300

    Rating star - icone no item.classificacao AVALIACOESPAGE; Menu Lateral acrescentado EstabelecimentosPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: 'HomePage' }
+      { title: 'Home', component: 'HomePage' },
+      { title: 'Estabelecimentos', component: 'EstabelecimentosPage' }
     ];
 
   }
--- src/pages/avaliacoes/avaliacoes.ts
+++ src/pages/avaliacoes/avaliacoes.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, Input } from '@angular/core';
+import { IonicPage, Item, ItemSliding, NavController, NavParams } from 'ionic-angular';
 import { AvaliacaoDTO } from '../../model/avaliacao.dto';
 import { AvaliacaoService } from '../../services/domain/avaliacao.service';
 
@@ -20,7 +20,7 @@ export class AvaliacoesPage {
     public avaliacaoService: AvaliacaoService) {
   }
 
-  ionViewDidLoad() {    
+  ionViewDidLoad() {
     let estabelecimento_id  = this.navParams.get('estabelecimento_id')
     this.avaliacaoService.findByEstabelecimentoAvaliacao(estabelecimento_id)
       .subscribe(response => {
@@ -31,5 +31,18 @@ export class AvaliacoesPage {
         console.log(error);
       });
   }
-  
+
 }
+
+
+
+// {
+//   "idAvaliacao": 4,
+//   "estabelecimentoId": 2,
+//   "nomeEstabelecimento": "Hospital Wakanda ",
+//   "dataCriacao": "23/11/2021 14:10",
+//   "descricao": "Foi bom o atendimento, mas poderia ser melhor",
+//   "classificacao": 3.0,
+//   "usuarioId": 4,
+//   "apelido": "Dali4"
+// }
